52218c44536e4d928aa50b2b06fa781f8bd695a4
Leonardo Matos <leomp120894@gmail.com>
2021-04-09 19:23:35 -0300

    Update index.js

--- .functions/index.js
+++ .functions/index.js
@@ -4,4 +4,7 @@ const { ssr } = require('@ecomplus/storefront-renderer/functions/')
 
 process.env.STOREFRONT_LONG_CACHE = 'true'
 
-exports.ssr = functions.https.onRequest((req, res) => ssr(req, res))
+exports.ssr = functions.https.onRequest((req, res) => {
+  console.log(req.url)
+  return ssr(req, res)
+})
