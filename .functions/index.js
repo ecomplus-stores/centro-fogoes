f04ceb7a0fe7e4f79e05984d835f838938d7fb83
Leonardo Matos <leomp120894@gmail.com>
2021-04-21 18:39:06 -0300

    build: updating with https://github.com/ecomplus/storefront-starter [skip ci]

--- .functions/index.js
+++ .functions/index.js
@@ -4,7 +4,4 @@ const { ssr } = require('@ecomplus/storefront-renderer/functions/')
 
 process.env.STOREFRONT_LONG_CACHE = 'true'
 
-exports.ssr = functions.https.onRequest((req, res) => {
-  // console.log(req.url)
-  return ssr(req, res)
-})
+exports.ssr = functions.https.onRequest((req, res) => ssr(req, res))
