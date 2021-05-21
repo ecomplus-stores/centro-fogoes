0a27e5a83816a535abb27741bae62e25e12db009
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-05-21 16:10:07 -0300

    chore: update header

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,3 @@
 // Add your custom JavaScript for storefront pages here.
+const nameIf = window.ecomPassport.customer.display_name ? window.ecomPassport.customer.display_name : 'Entre ou Cadastre-se'
+document.getElementById('nameIf').innerText = nameIf
