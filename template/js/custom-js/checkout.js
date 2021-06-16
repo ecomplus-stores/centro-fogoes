3cf92fcfa3891cf87c9c3bf08f065a843650d19e
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-06-16 17:14:03 -0300

    chore: final adjust checkout

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -1,1 +1,8 @@
 // Add your custom JavaScript for checkout here.
+storefront.on('widget:@ecomplus/widget-tag-manager', function () {
+    setTimeout(function () {
+        if(window.innerWidth < 767) {
+            $('.cart__discount').after($('.recommended-items'))
+        }
+    }, 500);
+  });
