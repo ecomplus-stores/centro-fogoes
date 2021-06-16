a35d575e5e90e539f4b09a1ec6199f446f813c9a
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-06-15 21:30:26 -0300

    chore: update site

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,3 +1,10 @@
 // Add your custom JavaScript for storefront pages here.
 const nameIf = window.ecomPassport.customer.display_name ? window.ecomPassport.customer.display_name : 'Minha Conta'
 document.getElementById('nameIf').innerText = nameIf
+const slider = document.querySelector('.banner-slider')
+const categories = document.getElementById('categories-row')
+const home = document.getElementById('page-home')
+
+if (window.innerWidth < 768 && home) {
+    slider.after(categories)
+}
